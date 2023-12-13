88e02444614281d4f270e650358761a94c1ae269
Omar Ninach <omarninach96@gmail.com>
2023-12-13 15:17:52 +0100

    fix issues and push to test

--- exercises/exercise3.py
+++ exercises/exercise3.py
@@ -30,30 +30,28 @@ class CSVLoader:
 
         try:
             # Fetch data directly from the URL using pandas
-            data = pd.read_csv(self.csv_url, sep = ';',encoding='ISO-8859-15',engine = 'python',skiprows=7, nrows=475,header=None)
-            mapping={0:'date', 1:'CIN',2:'name',12:'petrol', 22:'diesel', 32:'gas',42:'electro',52:'hybrid',62:'plugInHybrid',72:'others'}
-            df = data[list(mapping.keys())].rename(columns=mapping)
-            
-            
-            df['CIN'] = df['CIN'].astype(str)
+            data = pd.read_csv(self.csv_url, sep = ';',encoding='ISO-8859-15',engine = 'python',skiprows=7, nrows=476,header=None)
+            data = data.iloc[:, [0, 1, 2, 12, 22, 32, 42, 52, 62, 72]]
+            data.rename(columns={0: 'date', 1: 'CIN', 2: 'name', 12: 'petrol', 22: 'diesel', 32: 'gas', 42: 'electro', 52: 'hybrid', 62: 'plugInHybrid', 72: 'others'}, inplace=True)
 
-            # Keep rows where 'CIN' is a string with 5 characters
-            # df = df[df['CIN'].astype(str).match(r'^\d{5}$')]
-            cin_mask = (df['CIN'].apply(lambda x: len(x) == 5 and x.isdigit()))
-            # Apply the mask to filter rows
-            df = df[cin_mask]
-            
-            
-            columns_to_check = ['petrol', 'diesel', 'gas', 'electro', 'hybrid', 'plugInHybrid', 'others']
+            # Columns to filter
+            columns_to_filter = ['petrol', 'diesel', 'gas', 'electro', 'hybrid', 'plugInHybrid', 'others']
 
-            # Convert specified columns to numeric, handle non-numeric values as NaN, and fill NaN with 0
-            df[columns_to_check] = df[columns_to_check].applymap(lambda x: pd.to_numeric(x, errors='coerce')).fillna(0)
-            # Filter rows where values are greater than 0 after converting to integers
-            df = df[(df[columns_to_check].astype(int) > 0).all(axis=1)]
-            
+            for column in columns_to_filter:
+                filter_condition = (data[column] != "") & (data[column] != "-")
+                data = data[filter_condition]
 
+            data = data.astype(
+                {'date': str, 'CIN': str, 'name': str, 'petrol': int, 'diesel': int, 'gas': int, 'electro': int, 'hybrid': int, 'plugInHybrid': int, 'others': int})
+
+            data['CIN'] = data['CIN'].astype(str).str.zfill(5)
+
+            # Clip lower bounds to 0
+            data[columns_to_filter] = data[columns_to_filter].clip(lower=0)
+
+            data.dropna(inplace=True)
             # Use to_sql for bulk insert
-            df.to_sql(self.table_name, connection, if_exists='replace', index=False, method='multi')
+            data.to_sql(self.table_name, connection, if_exists='replace', index=False, method='multi')
 
             print("Data loaded successfully.")
         except Exception as e:
